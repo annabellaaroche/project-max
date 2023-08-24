8a45ab6940303fab860bce3aaa2faaeec4559b64
Annabella Aroche <110144837+annabellaaroche@users.noreply.github.com>
2023-08-23 22:24:22 -0600

    NewUser Model added to admin
    Added newUser Model to admin and set the configuration to create new users, filter, search, etc

--- project-max/api/admin.py
+++ project-max/api/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Owner,Especie,Raza,petSize,Pets,Vacuna,citaMedica
 from users.models import NewUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
+class UserAdminConfig(UserAdmin):
+    searh_fields = ('email','user_name','first_name')
+    list_filter = ('email','user_name','first_name','is_active','is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email','user_name','first_name','last_name','is_active','is_staff')
+    fieldsets =(
+        (None, {'fields': ('email','user_name','first_name','last_name',)}),
+        ('Permissions', {'fields': ('is_staff','is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email','user_name','first_name', 'last_name','password1','password2','is_active','is_staff')
+        }),
+    )
 admin.site.register(Owner)
 admin.site.register(Especie)
 admin.site.register(Raza)
@@ -11,4 +27,4 @@ admin.site.register(petSize)
 admin.site.register(Pets)
 admin.site.register(Vacuna)
 admin.site.register(citaMedica)
-admin.site.register(NewUser)
+admin.site.register(NewUser, UserAdminConfig)
