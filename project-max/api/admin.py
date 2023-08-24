ed6e782658834d0aa5849ac9714926b680c08200
Annabella Aroche <110144837+annabellaaroche@users.noreply.github.com>
2023-08-23 19:22:11 -0600

    Admin + Requirements.txt file created
    Admin Rutas - Requirements.txt File created with the requirements to install the app

--- project-max/api/admin.py
+++ project-max/api/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Owner
+from .models import Owner,Especie,Raza,petSize,Pets,Vacuna,citaMedica
 
 
 # Register your models here.
 
 admin.site.register(Owner)
+admin.site.register(Especie)
+admin.site.register(Raza)
+admin.site.register(petSize)
+admin.site.register(Pets)
+admin.site.register(Vacuna)
+admin.site.register(citaMedica)
