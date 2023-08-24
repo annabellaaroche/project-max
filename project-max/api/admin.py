07c941ac258dd1082bff7a811f6200cc933c1da7
Annabella Aroche <110144837+annabellaaroche@users.noreply.github.com>
2023-08-23 21:34:25 -0600

    UserModel + JWT Installation
    Created User Model, replaced Django default user model and installed JWT

--- project-max/api/admin.py
+++ project-max/api/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Owner,Especie,Raza,petSize,Pets,Vacuna,citaMedica
-
+from users.models import NewUser
 
 # Register your models here.
 
@@ -11,3 +11,4 @@ admin.site.register(petSize)
 admin.site.register(Pets)
 admin.site.register(Vacuna)
 admin.site.register(citaMedica)
+admin.site.register(NewUser)
---
+++ project-max/users/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _ #Library to transalate Json response or request like 'Email address'
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager#this let us override the default user table from django
+
+# Create your models here.
+
+#Users Model
+
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, email, user_name,first_name,password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email = email, user_name=user_name,first_name = first_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, user_name,first_name,password, **other_fields):
+            other_fields.setdefault('is_staff',True)
+            other_fields.setdefault('is_superuser',True)
+            other_fields.setdefault('is_active',True)
+        
+            if other_fields.get('is_staff') is not True:
+                raise ValueError(
+                    _('Superuser must be assigned to is_staff = True.')
+                )
+            if other_fields.get('is_superuser') is not True:
+                raise ValueError(
+                    _('Superuser must be assigned to is_superuser = True.')
+                )
+            return self.create_user(email,user_name,first_name,password, **other_fields)    
+
+class NewUser(AbstractBaseUser,PermissionsMixin):
+
+    id_user = models.AutoField(primary_key=True,unique=True)
+    email= models.EmailField(_('email address'),unique=True)
+    user_name = models.CharField(max_length=150,unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    start_date = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    objects = CustomAccountManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name','first_name']
+
+    def __str__(self):
+        return self.user_name
+
