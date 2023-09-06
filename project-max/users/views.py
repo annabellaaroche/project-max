6b4e04bfaafcfa75f11134e19d030a94406427d9
Annabella Aroche <110144837+annabellaaroche@users.noreply.github.com>
2023-09-05 21:44:33 -0600

    Blacklist JWT Implementation
    Implementacion de Backlist JWT Token

--- project-max/users/views.py
+++ project-max/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .serializers import RegisterUserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
@@ -16,3 +17,16 @@ class CustomUserCreate(APIView):
             if newuser:
                 return Response(data="User Created",status=status.HTTP_201_CREATED)
         return Response(reg_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(data="Logout Successful", status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
